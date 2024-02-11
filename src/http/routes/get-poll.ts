bcdb9fa6ac5f30900cabc2af0c373ff5ebe73059
Maicon Lourenço <maiconloure@gmail.com>
2024-02-10 23:52:38 -0300

    feat: added websocket poll routes

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 import { prisma } from "../../lib/prisma";
 import { FastifyInstance } from "fastify";
+import { redis } from "../../lib/redis";
+import { title } from "process";
 
 export async function getPoll(app: FastifyInstance) {
   app.get('/polls/:pollId', async (request, reply) => {
@@ -24,6 +26,31 @@ export async function getPoll(app: FastifyInstance) {
       }
     })
 
-    return reply.send({ poll })
+    if (!poll) {
+      return reply.status(400).send({ message: "poll not found!" })
+    }
+
+    const result = await redis.zrange(pollId, 0, -1, 'WITHSCORES')
+
+    const votes = result.reduce((acc, curr, index) => {
+      if (index % 2 === 0) {
+        const score = result[index + 1]
+        Object.assign(acc, { [curr]: Number(score) })
+      }
+
+      return acc
+    }, {} as Record<string, number>)
+
+    return reply.send({ poll: {
+      id: poll.id,
+      title: poll.title,
+      options: poll.options.map(option => {
+        return {
+          id: option.id,
+          title: option.title,
+          score: (option.id in votes) ? votes[option.id] : 0,
+        }
+      }),
+    } })
   })
 }
